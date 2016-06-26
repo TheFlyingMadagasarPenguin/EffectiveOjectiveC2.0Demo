8f08ee79ccedc170a123b792aa89da0d3340d388
TheBaymax <just_for_h264@163.com>
2016-06-26 17:23:45 +0800

    Initial Commit

---
+++ EffectiveOjectiveC2.0Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EffectiveOjectiveC2.0Demo
+//
+//  Created by TheFlyingPenguin on 16/6/26.
+//  Copyright © 2016年 TheFlyingPenguin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EffectiveOjectiveC2.0Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EffectiveOjectiveC2.0Demo
+//
+//  Created by TheFlyingPenguin on 16/6/26.
+//  Copyright © 2016年 TheFlyingPenguin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
